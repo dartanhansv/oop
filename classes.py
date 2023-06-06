6b6c94f04a41eaee490ebfa6c1603841793ce566
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-06 11:32:03 -0300

    v3 fuctions

--- classes.py
+++ classes.py
@@ -23,7 +23,19 @@ employee1 = {
     }
 
 employee2 = {"name": "Lauren", "age": 44, "position": "tester", "salary": 1000}
+
+
+def increase_salary(employee, percent):
+    employee['salary'] += employee['salary'] * (percent/100)
+
+
+def employee_info(employee):
+    print(f"{employee['name']} is {employee['age']} years old. Employee is a \
+{employee['position']} with the salary of ${employee['salary']} ")
+
+
 employees = [employee1, employee2]
 
+increase_salary(employee2, 20)
 for e in employees:
-    print(f"{e['name']}s' salary is ${e['salary']}")
+    employee_info(e)
