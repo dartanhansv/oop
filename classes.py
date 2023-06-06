967bc5e8b1d62381fa19bfb902b2c8735a6310a9
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-06 10:48:07 -0300

    v2 dicitonary

--- classes.py
+++ classes.py
@@ -1,7 +1,29 @@
-employee1 = ["Ji-Soo", 38, "developer", 1200]
-employee2 = ["Lauren", 44, "tester", 1000]
+# First version:
+# This is a small and simple example,
+# that wouldn't escalate well enough for big applications.
+#
+# employee1 = ["Ji-Soo", 38, "developer", 1200]
+# employee2 = ["Lauren", 44, "tester", 1000]
+# employees = [employee1, employee2]
+#
+# for e in employees:
+#    print(f"{e[0]}s' salary is ${e[3]}")
 
+
+# 2nd version -  with Dictionaries
+# Now we do not need to remember which index
+# is used for which piece of data.
+# I can refer to the data by name
+
+employee1 = {
+    "name": "Ji-Soo",
+    "age": 38,
+    "position": "developer",
+    "salary": 1200
+    }
+
+employee2 = {"name": "Lauren", "age": 44, "position": "tester", "salary": 1000}
 employees = [employee1, employee2]
 
 for e in employees:
-    print(f"{e[0]}s' salary is ${e[3]}")
+    print(f"{e['name']}s' salary is ${e['salary']}")
