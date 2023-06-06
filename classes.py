5ca4538b179e4584e9643cc57851906275a970c3
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-06 10:30:06 -0300

    add classes.py

---
+++ classes.py
@@ -0,0 +1,7 @@
+employee1 = ["Ji-Soo", 38, "developer", 1200]
+employee2 = ["Lauren", 44, "tester", 1000]
+
+employees = [employee1, employee2]
+
+for e in employees:
+    print(f"{e[0]}s' salary is ${e[3]}")
