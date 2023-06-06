3473b3b36969aaca2233dcf1672741cb9081354a
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-06 11:50:07 -0300

    v4 init_employee

--- classes.py
+++ classes.py
@@ -25,6 +25,15 @@ employee1 = {
 employee2 = {"name": "Lauren", "age": 44, "position": "tester", "salary": 1000}
 
 
+def init_employee(name, age, position, salary):
+    return {
+        "name": name,
+        "age": age,
+        "position": position,
+        "salary": salary
+    }
+
+
 def increase_salary(employee, percent):
     employee['salary'] += employee['salary'] * (percent/100)
 
@@ -34,7 +43,8 @@ def employee_info(employee):
 {employee['position']} with the salary of ${employee['salary']} ")
 
 
-employees = [employee1, employee2]
+employee3 = init_employee("Mateo", 38, "developer", 2000)
+employees = [employee1, employee2, employee3]
 
 increase_salary(employee2, 20)
 for e in employees:
