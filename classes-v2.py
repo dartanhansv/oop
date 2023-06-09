98a8476a334cf6047d90dfbbc81fdbb09b44b2c5
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-08 22:55:34 -0300

    starting v2

---
+++ classes-v2.py
@@ -0,0 +1,25 @@
+class Employee:
+    def __init__(self, name, age, position, salary):
+        self.name = name
+        self.age = age
+        self.position = position
+        self.salary = salary
+
+
+employee1 = Employee("Ji-Soo", 38, "developer", 1200)
+employee2 = Employee("Lauren", 44, "tester", 1000)
+
+print(employee1.name)
+print(employee2.name)
+
+# Terminology
+# We could call employee1 and employee2 objects
+# but everything is object in Python
+# so we call them employee instances
+#
+# Instantiation
+# Is the pocess of using a class
+# to create a new object
+#
+# This code uses the Employee class
+# to instantiate new employee instaces
