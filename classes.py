8358a99c4242d5327e4def136ab5add4bdff31ea
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-08 22:48:11 -0300

    __init__

--- classes.py
+++ classes.py
@@ -1,51 +1,13 @@
-# First version:
-# This is a small and simple example,
-# that wouldn't escalate well enough for big applications.
-#
-# employee1 = ["Ji-Soo", 38, "developer", 1200]
-# employee2 = ["Lauren", 44, "tester", 1000]
-# employees = [employee1, employee2]
-#
-# for e in employees:
-#    print(f"{e[0]}s' salary is ${e[3]}")
+class Employee:
+    def __init__(self, name, age, position, salary):
+        self.name = name
+        self.age = age
+        self.position = position
+        self.salary = salary
 
 
-# 2nd version -  with Dictionaries
-# Now we do not need to remember which index
-# is used for which piece of data.
-# I can refer to the data by name
+employee1 = Employee("Ji-Soo", 38, "developer", 1200)
+employee2 = Employee("Lauren", 44, "tester", 1000)
 
-employee1 = {
-    "name": "Ji-Soo",
-    "age": 38,
-    "position": "developer",
-    "salary": 1200
-    }
-
-employee2 = {"name": "Lauren", "age": 44, "position": "tester", "salary": 1000}
-
-
-def init_employee(name, age, position, salary):
-    return {
-        "name": name,
-        "age": age,
-        "position": position,
-        "salary": salary
-    }
-
-
-def increase_salary(employee, percent):
-    employee['salary'] += employee['salary'] * (percent/100)
-
-
-def employee_info(employee):
-    print(f"{employee['name']} is {employee['age']} years old. Employee is a \
-{employee['position']} with the salary of ${employee['salary']} ")
-
-
-employee3 = init_employee("Mateo", 38, "developer", 2000)
-employees = [employee1, employee2, employee3]
-
-increase_salary(employee2, 20)
-for e in employees:
-    employee_info(e)
+print(employee1.name)
+print(employee2.name)
