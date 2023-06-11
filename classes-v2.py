267282e1de6b94545af4419399eb48c4a88f67d5
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-11 15:08:41 -0300

    fix fuct with dot notation

--- classes-v2.py
+++ classes-v2.py
@@ -6,18 +6,18 @@ class Employee:
         self.salary = salary
 
     def increase_salary(employee, percent):
-        employee['salary'] += employee['salary'] * (percent/100)
+        employee.salary += employee.salary * (percent/100)
 
 
     def employee_info(employee):
-        print(f"{employee['name']} is {employee['age']} years old. Employee is a \
-    {employee['position']} with the salary of ${employee['salary']} ")
+        print(f"{employee.name} is {employee.age} years old. Employee is a \
+    {employee.position} with the salary of ${employee.salary} ")
 
 
 
 employee1 = Employee("Ji-Soo", 38, "developer", 1200)
 employee2 = Employee("Lauren", 44, "tester", 1000)
 
-Employee.employee_info(employee2.__dict__)
-Employee.increase_salary(employee2.__dict__, 20)
-Employee.employee_info(employee2.__dict__)
+Employee.employee_info(employee2)
+Employee.increase_salary(employee2, 20)
+Employee.employee_info(employee2)
