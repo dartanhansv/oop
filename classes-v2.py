cdb983e577075ad49a746d57d6768ef2acf1d811
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-11 15:24:30 -0300

    linting

--- classes-v2.py
+++ classes-v2.py
@@ -10,11 +10,9 @@ class Employee:
     def increase_salary(employee, percent):
         employee.salary += employee.salary * (percent/100)
 
-
     def info(self):
         print(f"{self.name} is {self.age} years old. Employee is a \
     {self.position} with the salary of ${self.salary} ")
-
 
 
 employee1 = Employee("Ji-Soo", 38, "developer", 1200)
@@ -23,4 +21,3 @@ employee2 = Employee("Lauren", 44, "tester", 1000)
 employee2.info()
 employee2.increase_salary(20)
 employee2.info()
-  
