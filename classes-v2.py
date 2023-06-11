783887bdf21a0387559b242eeb72e199ffce4f5c
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-11 14:57:43 -0300

    reset

--- classes-v2.py
+++ classes-v2.py
@@ -1,24 +1,13 @@
-# class Employee:
-#     def __init__(self, name, age, position, salary):
-#         self.name = name
-#         self.age = age
-#         self.position = position
-#         self.salary = salary
-
-
-# employee1 = Employee("Ji-Soo", 38, "developer", 1200)
-# employee2 = Employee("Lauren", 44, "tester", 1000)
-
-# print(employee1.name)
-# print(employee2.name)
-
 class Employee:
-    def __init__(self):
-        self.name = "Ji-Soo"
-        self.age = 38
-        self.position = "developer"
-        self.salary = 1200
+    def __init__(self, name, age, position, salary):
+        self.name = name
+        self.age = age
+        self.position = position
+        self.salary = salary
 
 
-e = Employee()
-print(e.__class__)
+employee1 = Employee("Ji-Soo", 38, "developer", 1200)
+employee2 = Employee("Lauren", 44, "tester", 1000)
+
+print(employee1.name)
+print(employee2.name)
