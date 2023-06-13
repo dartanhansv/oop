ff41d67369e4aeb66b1cdacd7691d94c78b7c4cc
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-13 09:41:58 -0300

    fix init self.set_salary

--- classes-v5.py
+++ classes-v5.py
@@ -3,7 +3,7 @@ class Employee:
         self.name = name
         self.age = age
         self.position = position
-        self.set_salary = salary
+        self.set_salary(salary)
 
     def increase_salary(employee, percent):
         employee.salary += employee.salary * (percent/100)
