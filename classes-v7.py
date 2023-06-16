c315b3f92129fe7a24dafda866cd59761ad999e4
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-16 20:16:52 -0300

    v7 - remove comments

--- classes-v7.py
+++ classes-v7.py
@@ -4,9 +4,7 @@ class Employee:
         self.age = age
         self.position = position
         self.salary = salary
-        # self.annual_salary = 12 * salary
-        # This way, annual salary is always calculated
-        self._annual_salary = None  # for caching
+        self._annual_salary = None
 
     def increase_salary(self, percent):
         self.salary += self.salary * (percent/100)
@@ -31,26 +29,14 @@ class Employee:
     def salary(self, salary):
         if salary < 1000:
             raise ValueError('Minimum wage is $1000')
-        # Fix the not updated annual_salary issue
         self._annual_salary = None
         self._salary = salary
-
-    # @property
-    # def annual_salary(self):
-    #     return self.salary * 12
-    # # computed property: computation with attributes from the same instance
-    # # Now, annual salary is calculated only when we need to access it
 
     @property
     def annual_salary(self):
         if self._annual_salary is None:
             self._annual_salary = self.salary * 12
         return self._annual_salary
-    # if annual_salary is None, calculate
-    # if not, if annual_salary was already calculated, return its values
-    # But if the salary changed, annual salary wont be recalculated
-    # To fix that, we need to set annual_salary to None everytime
-    # someone updates the salary - line 35
 
 
 employee1 = Employee("Ji-Soo", 38, "developer", 1200)
