304f3bfd19f74a1331c78cf90b2554d2d2428541
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-16 20:35:49 -0300

    cleanup

--- classes-v8.py
+++ classes-v8.py
@@ -14,14 +14,6 @@ class Tester(Employee):  # Inherit methods from the Employee class
     pass
 
 
-# To validate Class Inheritance
-# Create a new Tester instance with the Tester Class
 employee1 = Tester("Lauren", 44, 1000)
-
-# Increase the salary of the Tester instance using the
-# increase_salary function/method of the Employee Class
-
 employee1.increase_salary(20)
-
-# Print the new salary
 print(employee1.salary)
