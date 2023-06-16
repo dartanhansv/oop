31b7b71150ea6d785422b4eb60c169c4f8ad2b1e
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-16 20:33:05 -0300

    class inheritance validation

--- classes-v7.py
+++ classes-v7.py
@@ -10,5 +10,18 @@ class Employee:
         self.salary += self.salary * (percent/100)
 
 
-class Tester(Employee):
+class Tester(Employee):  # Inherit methods from the Employee class
     pass
+
+
+# To validate Class Inheritance
+# Create a new Tester instance with the Tester Class
+employee1 = Tester("Lauren", 44, 1000)
+
+# Increase the salary of the Tester instance using the
+# increase_salary function/method of the Employee Class
+
+employee1.increase_salary(20)
+
+# Print the new salary
+print(employee1.salary)
