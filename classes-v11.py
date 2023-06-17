ede7d2fbc05d46965d63dc48e165bbc70cf26e90
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-17 17:13:43 -0300

    cleanup comments

--- classes-v11.py
+++ classes-v11.py
@@ -30,32 +30,3 @@ employee1.increase_salary(20)
 employee2.increase_salary(20, 30)
 print(employee1.salary)
 print(employee2.salary)
-
-# Instance returns True if the 1st argument is
-# an instance of the class from the 2nd argument
-
-# employee is a Tester instance and it is also
-# a instance of Employee as Tester is the
-# subclass of Employee
-print(isinstance(employee1, Tester))
-print(isinstance(employee1, Employee))
-
-# employee is not an instance of Developer,
-# so it will return False
-print(isinstance(employee1, Developer))
-print("\n")
-
-# "issubclass" checks for class inheritance
-# issubclass(subclass, master class)
-# True = the 1st arg is a subclass of the 2nd arg
-print(issubclass(Developer, Employee))
-print(issubclass(Tester, Employee))
-print(issubclass(Employee, Developer))
-print(issubclass(Employee, Tester))
-print(issubclass(Developer, object))
-print(issubclass(Tester, object))
-print(issubclass(Employee, object))
-
-# Developer and Tester are subclasses of Employee
-# Employee is not a subclasses of Tester/Developer
-# All classes are subclasses of class object
