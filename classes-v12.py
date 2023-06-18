56f40724a7f26986552a4f66ced4f5c6d354ea36
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-18 19:49:18 -0300

    new attribut: project

--- classes-v12.py
+++ classes-v12.py
@@ -17,14 +17,26 @@ class Tester(Employee):
         print("Tests are done.")
 
 
+# This Developer Class inharent methods from the Employee Class
+# Meaning that it will create new instances(Developers employees)
+# using the attributes defined on the init method of Employee Class
+# What if the Subclass need more attributes that the parent class?
 class Developer(Employee):
+    # Just override the init method, adding the new attribute:
+    def __init__(self, name, age, salary, project):
+        # And to avoid code repetition - remember DRY -
+        # use "super" to invoke the init method from the
+        # Employee Class
+        super().__init__(name, age, salary)
+        self.project = project
+
     def increase_salary(self, percent, bonus=0):
         super().increase_salary(percent)
         self.salary += bonus
 
 
 employee1 = Tester("Lauren", 44, 1000)
-employee2 = Developer("Ji-Soo", 38, 1000)
+employee2 = Developer("Ji-Soo", 38, 1000, "Project ABC")
 
 
 employee1.increase_salary(20)
