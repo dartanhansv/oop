c30c8dd5e491afc8d0f4787596b9eb86b1a7cf93
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-18 19:33:57 -0300

    cleanup

--- classes-v12.py
+++ classes-v12.py
@@ -1,5 +1,5 @@
 # Implementing Class Inheritance
-# Extending Parent Class Instance methods with super
+
 
 class Employee:
     def __init__(self, name, age, salary):
@@ -18,25 +18,9 @@ class Tester(Employee):
 
 
 class Developer(Employee):
-    # Define the same method present in the Employee Class
-    # Than repeat the same calculation. This is hard
-    # to mantain.
-    # def increase_salary(self, percent, bonus=0):
-    #     self.salary += self.salary * (percent/100)
-    #     self.salary += bonus
-
-    # So how to access the method from the parent Class?
-    # Using the super fuction!
     def increase_salary(self, percent, bonus=0):
-        super().increase_salary(percent)  # What happens in the background is:
-        # Employee.increase_salary(self,percent) but it wouldn't like
-        # if the code was like that because self would refer to the
-        # increase_salary of the Developer Class, not from Employee
-        # as intended
+        super().increase_salary(percent)
         self.salary += bonus
-
-    # This is a "DRY" code
-    " DRY = Don't Repeat yourself"
 
 
 employee1 = Tester("Lauren", 44, 1000)
