9db99749e95566a32e39474dd36141fe54c0d1f5
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-22 12:08:20 -0300

    comments worth to remember

--- classes-v14.py
+++ classes-v14.py
@@ -31,3 +31,12 @@ print(Employee.minimum_wage)
 
 # Employee instances also have access to the class attributes
 print(e.minimum_wage)
+
+
+# Lookup chain
+# When you try to access a certain attribute from an instance,
+# the instance will first look into its own dictionary to find it.
+# If it's not there, it will look inside of its class.
+# If this Employee instance had the attibute with the same neme,
+# "minimum_wage", then that value would be returned instead of
+# the one the class.
