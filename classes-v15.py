4147563c587640bd4af2a080ed4a9fc689bf3ef9
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-24 18:12:00 -0300

    flake8

--- classes-v15.py
+++ classes-v15.py
@@ -14,11 +14,11 @@ class Employee:
         cls.minimum_wage = new_wage
 
     @classmethod
-    def new_employee(cls, name, dob): # dob = date of birth
+    def new_employee(cls, name, dob):  # dob = date of birth
         now = date.today()
-        age = now.year - dob.year - ((now.month, now.day) <(dob.month, dob.day))
+        age = now.year - dob.year - ((now.month, now.day)
+                                     < (dob.month, dob.day))
         return cls(name, age, cls.minimum_wage)
-
 
     def __init__(self, name, age, salary):
         self.name = name
