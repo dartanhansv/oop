937d001da91a7e8a1889e23f171618de764f9aff
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-24 16:12:05 -0300

    v15 cleanup

--- classes-v15.py
+++ classes-v15.py
@@ -3,7 +3,6 @@
 # Defining Class attributes
 
 class Employee:
-    # a example of Class method
     minimum_wage = 1000
 
     def __init__(self, name, age, salary):
@@ -20,7 +19,6 @@ class Employee:
 
     @salary.setter
     def salary(self, salary):
-        # call the Class method directly
         if salary < Employee.minimum_wage:
             raise ValueError('Minimum wage is ${Employee.minimum_wage}')
         self._salary = salary
@@ -28,15 +26,6 @@ class Employee:
 
 e = Employee("Ji-Soo", 38, 1000)
 print(Employee.minimum_wage)
-
-# Employee instances also have access to the class attributes
 print(e.minimum_wage)
 
 
-# Lookup chain
-# When you try to access a certain attribute from an instance,
-# the instance will first look into its own dictionary to find it.
-# If it's not there, it will look inside of its class.
-# If this Employee instance had the attibute with the same neme,
-# "minimum_wage", then that value would be returned instead of
-# the one the class.
