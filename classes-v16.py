39972ab5762d34d50cd34d01fecbddf6f4f78b39
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-25 06:06:13 -0300

    adding Project class

--- classes-v16.py
+++ classes-v16.py
@@ -1,9 +1,16 @@
 # 7.. Using Data Classes
 # 7.1. Introducing Data Classes
 
+class Project:
+    def __init__(self, name, payment, client):
+        self.name = name
+        self.payment = payment
+        self.client = client
+
+
 class Employee:
-    def __init__(self, name, age, salary):
+    def __init__(self, name, age, salary, project):
         self.name = name
         self.age = age
         self.salary = salary
-
+        self.salary = project
