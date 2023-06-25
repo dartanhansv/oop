9f5fd1987b7dbac9beacda8a914bfa5f3b9a50bd
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-06-25 06:51:42 -0300

    cleanup and topic

--- classes-v18.py
+++ classes-v18.py
@@ -1,5 +1,5 @@
 # 7.. Using Data Classes
-# 7.2. Type hinting of instance attributes
+# 7.3. Implementing Slots and Methods
 
 from dataclasses import dataclass
 from typing import Any
@@ -7,9 +7,9 @@ from typing import Any
 
 @dataclass
 class Project:
-    name: str  # Type hints: it is hinting which type of value is expected
+    name: str
     payment: int
-    client: Any  # To avoind the need of hinting, you can use Any
+    client: Any
 
 
 class Employee:
@@ -19,21 +19,6 @@ class Employee:
         self.salary = salary
         self.project = project
 
-
-# Python will not check the type of each attribute.
-# We need additional tools to do that, like mypy
-# Install: $ python3 -m pip install mypy
-# Check:   $ mypy program.py
-# Real Example:
-# $ mypy classes-v17.py
-# classes-v17.py:27: error: Argument 2 to "Project" has incompatible type
-# "str"; expected "int"  [arg-type]
-# Found 1 error in 1 file (checked 1 source file)
-
-
-# Resolving the error and rechecking with mypy
-# $ mypy classes-v17.py
-# Success: no issues found in 1 source file
 
 p = Project("Django App", 2000, "Globomantics")
 e = Employee("Ji-Soo", 38, 1200, p)
